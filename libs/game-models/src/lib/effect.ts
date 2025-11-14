6bceef583795d0330f2cd794eb6a1d20ec03013d
dirk.wolthuis <dirk.wolthuis@ns.nl>
2025-11-14 20:36:12 +0100

    Define Damage Effect

--- libs/game-models/src/lib/effect.ts
+++ libs/game-models/src/lib/effect.ts
@@ -1,1 +1,24 @@
-export interface Effect {}
+export type Effect = DamageEffect | HealEffect;
+
+export interface DamageEffect {
+  type: EffectType.DEAL_DAMAGE;
+  target: TargetType;
+  value: number;
+}
+
+export interface HealEffect {
+  type: EffectType.HEAL;
+  target: TargetType.SELF;
+  value: number;
+}
+
+export enum EffectType {
+  DEAL_DAMAGE,
+  HEAL,
+}
+
+export enum TargetType {
+  SELF,
+  PLAYER,
+  OPPONENT,
+}
